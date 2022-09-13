71a7ebadfef7f285fb301bd9eaec953f27dbf68d
Я <aleksey12free@mail.ru>
2022-09-13 21:51:23 +0400

    Оптимизация кода

--- src/Components/UI/SPBItem.jsx
+++ src/Components/UI/SPBItem.jsx
@@ -4,13 +4,13 @@ import classes from "./SPBItem.module.css";
 const SPBItem = ({ children, active, result }) => {
   let counterClass = classes.counter;
   let itemClass = classes.item;
-  if (active) counterClass += " " + classes.active;
-  if (result == "y") {
-    itemClass += " " + classes.yes;
-    counterClass += " " + classes.yes;
-  } else if (result == "n") {
-    itemClass += " " + classes.no;
-    counterClass += " " + classes.no;
+  if (active) counterClass += ` ${classes.active}`;
+  if (result === "y") {
+    itemClass += ` ${classes.yes}`;
+    counterClass += ` ${classes.yes}`;
+  } else if (result === "n") {
+    itemClass += ` ${classes.no}`;
+    counterClass += ` ${classes.no}`;
   }
 
   return (
